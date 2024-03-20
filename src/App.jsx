a75b0f656b14892efcbb7aaf0617a9f3081e8844
Jack Norkus <norkusjr@gmail.com>
2024-03-20 13:14:26 -0500

    finished GA6

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,28 @@
 import Buttons from "./components/Buttons";
 import Table from "./components/Table";
+import Banner from "./components/Banner";
+import { useState } from "react";
+import satData from "./components/satData";
 
 
 function App() {
+  const [sat, setSat] = useState(satData); //sat will be used to compare changes in state. setSat is a function that will be used to update the state.
+  const displaySats = [...new Set(satData.map((data) => data.orbitType))]; //The Set object lets you store UNIQUE values of any type.
+  const filterByType = (currentType) => {
+    const displaySats = satData.filter((newSatDisplay) => { //A callback function, like newSatDisplay, is a function passed into another function as an argument, which is then invoked inside the outer function to complete some kind of routine or action.
+      return newSatDisplay.orbitType === currentType;
+    });
+    setSat(displaySats);
+  };
   return (
     <div>
-      <Buttons />
-      <Table />
+      <Banner />
+      <Buttons 
+        filterByType={filterByType}
+        setSat={setSat}
+        displaySats={displaySats}
+      />
+      <Table sat={sat} />
     </div>
   );
 }
--- src/components/Table.jsx
+++ src/components/Table.jsx
@@ -3,13 +3,23 @@ const Table = ({ sat }) => {
     <table>
     <thead>
      <tr>
-       <th>Header TBD</th>
+       <th>Name</th>
+       <th>Type of Satellite</th>
+       <th>Launch Date</th>
+       <th>Status</th>
      </tr>
      </thead>
      <tbody>
-     <tr>
-       <td>Row Data TBD</td>
-     </tr>
+     {sat.map((data, id) => { //data and id need to be passed in the right spots!!!
+      return (
+        <tr key={id}>
+          <td>{data.name}</td>
+          <td>{data.type}</td>
+          <td>{data.launchDate}</td>
+          {data.operational ? <td>active</td> : <td>inactive</td>}
+        </tr>
+      );
+     })}
      </tbody>
    </table>
   );
